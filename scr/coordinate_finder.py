52c0c568476cc87d6aead031c6888c047af1b3b8
Lore <lorenyanski@gmail.com>
2026-02-21 19:43:02 +0100

    i am restarted

--- scr/coordinate_finder.py
+++ scr/coordinate_finder.py
@@ -1,10 +1,13 @@
 from pyautogui import position
+from time import sleep
 from config import DATA_DIR, COORD_FILE, whatsapp_groupid
 from pywhatkit.core.core import _web, close_tab
 
 _web(receiver=whatsapp_groupid, message="")
 input("Press Enter when your mouse is on the correct position...")
+sleep(1)
 with open(file=(DATA_DIR/COORD_FILE), mode="w+") as f:
     pos = position()
     f.writelines([str(pos.x), "\n", str(pos.y)])
+    print("Recorded: " + str(pos))
 close_tab()
